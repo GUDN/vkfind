6a5dcc3778f754132d20a9a8b09e88f2d734abbd
Daniil Novoselov <gudnmail@gmail.com>
2021-01-19 18:10:39 +0500

    add searchbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
-<h1>VKFind App</h1>
+<script lang="ts">
+  import Searchbar from './Searchbar.svelte'
+
+  function onSearchbar(event: CustomEvent) {
+    const value = event.detail as string
+    console.log(`Searching ${value}...`)
+  }
+</script>
+
+<main class="container">
+  <h1>VKFind App</h1>
+  <Searchbar on:search={onSearchbar} />
+</main>
---
+++ src/Searchbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+  export let value: string = ''
+
+  function onSubmit() {
+    dispatch('search', value)
+    value = ''
+  }
+</script>
+
+<form class="container" on:submit|preventDefault={onSubmit}>
+  <div class="row valign-wrapper">
+    <div class="input-field col s9">
+      <input placeholder="Enter name..." bind:value />
+    </div>
+    <button type="submit" class="btn col s2 waves-effect light-blue">
+      Search
+      <i class="material-icons right">search</i>
+    </button>
+    <div class="col s1">
+      <button class="btn-small waves-effect blue accent-4">
+        <i class="material-icons">fullscreen</i>
+      </button>
+    </div>
+  </div>
+</form>
+
+<style>
+  form {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+
+  div.row {
+    margin-bottom: 0;
+  }
+
+  input:focus {
+    border-bottom: 1px solid #03a9f4 !important;
+    box-shadow: 1px solid 0 0 #03a9f4 !important;
+  }
+</style>
